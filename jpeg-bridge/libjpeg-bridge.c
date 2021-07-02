1bc82f8feaa9ec7bf5854a70703a0a8c2839a8b1
Nemo <me@captnemo.in>
2021-07-02 17:53:23 +0530

    Fix hybris path

--- jpeg-bridge/libjpeg-bridge.c
+++ jpeg-bridge/libjpeg-bridge.c
@@ -1,14 +1,12 @@
-
-
 #include <stddef.h>
 #include <stdio.h>
 
-#include <dlfcn.h>
+#include <hybris/common/binding.h>
 
 #include <jinclude.h>
 #include <jpeglib.h>
 
-#include <hybris/internal/binding.h>
+#include <hybris/common/binding.h>
 
 
 HYBRIS_LIBRARY_INITIALIZE(libjpeg, "libjpeg2.so")
